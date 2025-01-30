5ee9880ff11f5fb8f14cad6d553b6a2f8cc67b9b
Ana Clara Souza <105755289+anaclpsouza@users.noreply.github.com>
2025-01-29 22:05:51 -0300

    Add files via upload

---
+++ Fila/Aeroporto/Fila.java
@@ -0,0 +1,31 @@
+public class Fila {
+    int tam = 0;
+    private No cabeca = new No();
+    private No cauda = cabeca;
+    private No el;
+
+    public void enfileirar(String aviao) {
+        el = new No(aviao, null);
+        if (tam != 0) {
+            cauda.proximo = el;
+        } else {
+            cabeca = el;
+        }
+        cauda = el;
+        tam++;
+    }
+
+    public String desenfileirar() throws Exception {
+        if (isEmpty()) {
+            throw new Exception("Sua fila está vazia");
+        }
+        String el = cabeca.elemento;
+        cabeca = cabeca.proximo;
+        tam--;
+        return el;
+    }
+
+    public boolean isEmpty() {
+        return tam == 0;
+    }
+}
