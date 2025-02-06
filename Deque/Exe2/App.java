fcadf1dbd8c409ddc8f150938009fd5dca6a5a06
Ana Clara Souza <105755289+anaclpsouza@users.noreply.github.com>
2025-02-06 09:21:01 -0300

    Update App.java

--- Deque/Exe2/App.java
+++ Deque/Exe2/App.java
@@ -8,9 +8,19 @@ public class App {
         double valor = 0;
 
         System.out.print("Valor do financiamento: ");
-        valor = sc.nextDouble();
+        try {
+            valor = sc.nextDouble();
+        } catch (Exception e) {
+            System.out.println("Digite somente números");
+            System.exit(0);
+        }
         System.out.print("Em quantas parcelas? ");
-        nParcelas = sc.nextInt();
+        try {
+            nParcelas = sc.nextInt();
+        } catch (Exception e) {
+            System.out.println("Digite apenas números inteiros");
+            System.exit(0);
+        }
         double valorParcelas = valor / nParcelas;
         for (int i = 1; i <= nParcelas; i++) {
             double juros = ((valorParcelas * 0.01) * i);
@@ -26,15 +36,16 @@ public class App {
             } else {
                 System.out.println("Parcelas:");
                 deque.print();
+                System.out.println(" ");
             }
             System.out.printf("Ações: \n 1. Pagar parcela atual \n 2. Pagar última parcela \n 3. Sair \n");
-            int op = sc.nextInt();
+            String op = sc.next();
             System.out.println(" ");
             switch (op) {
-                case 1:
+                case "1":
                     System.out.println("Valor do pagamento: " + deque.removeFirst().getValorParcelas());
                     break;
-                case 2:
+                case "2":
                     if (deque.size() > 1) {
                         Parcela p = deque.getLast();
                         double parcela = p.getValorParcelas() - p.getJuros();
@@ -44,11 +55,12 @@ public class App {
                         System.out.println("Valor do pagamento: " + deque.removeFirst().getValorParcelas());
                     }
                     break;
-                case 3:
+                case "3":
                     a = false;
                     System.exit(0);
                     break;
                 default:
+                    System.out.println("Opção inválida, nenhuma ação foi realizada.");
                     break;
             }
         }
