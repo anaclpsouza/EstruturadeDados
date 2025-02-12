ae49cefe5a0ad8628c783c8f676e082ea4bb11e9
Ana Clara Souza <anaclarap03@gmail.com>
2025-02-12 20:29:25 -0300

    Update HashTable.java

--- Hash/HashTable.java
+++ Hash/HashTable.java
@@ -1,27 +1,27 @@
 package Exe1;
-
 public class HashTable {
     public static final int M = 13;
     private Livro tabela[];
-    public HashTable(){
+
+    public HashTable() {
         tabela = new Livro[M];
     }
 
-    private int hash (String chave){
+    private int hash(String chave) {
         int h = 0;
         for (int i = 0; i < chave.length(); ++i)
             h = (31 * h + chave.charAt(i)) % M;
         return h;
     }
 
-    public Livro get(String chave){
+    public Livro get(String chave) {
         int hash;
         int salto;
-   
+
         for (salto = 0; salto < tabela.length; salto++) {
             hash = (hash(chave) + salto) % tabela.length;
 
-            if (tabela[hash].getISBN().equals(chave)) {
+            if (tabela[hash] != null && tabela[hash].getISBN().equals(chave)) {
                 return tabela[hash];
             }
         }
@@ -31,7 +31,7 @@ public class HashTable {
     public void putAberto(Livro l) throws Exception {
         int salto;
         int hash;
-        
+
         for (salto = 0; salto < tabela.length; salto++) {
             hash = (hash(l.getISBN()) + salto) % tabela.length;
 
@@ -50,11 +50,11 @@ public class HashTable {
     public Livro remove(String chave) {
         int hash;
         int salto;
-   
+
         for (salto = 0; salto < tabela.length; salto++) {
             hash = (hash(chave) + salto) % tabela.length;
 
-            if (tabela[hash].getISBN().equals(chave)) {
+            if (tabela[hash] != null && tabela[hash].getISBN().equals(chave)) {
                 Livro livro = tabela[hash];
                 tabela[hash] = null;
                 return livro;
@@ -62,13 +62,16 @@ public class HashTable {
         }
         return null;
     }
-   /*  public void status(){
-        for (int i=0;i<M;++i){
-            if (tabela[i]!=null){
-                Livro liv = tabela[i];
-                System.out.printf("A posição %d possui o livro %s\n",i,liv.getTitulo());
-            } else
-                System.out.printf("A posição %d não possui livros cadastrados\n",i);
-        }
-    } */
+
+    /*
+     * public void status(){
+     * for (int i=0;i<M;++i){
+     * if (tabela[i]!=null){
+     * Livro liv = tabela[i];
+     * System.out.printf("A posição %d possui o livro %s\n",i,liv.getTitulo());
+     * } else
+     * System.out.printf("A posição %d não possui livros cadastrados\n",i);
+     * }
+     * }
+     */
 }
